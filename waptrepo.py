4c11bad3cc1c3095dd91322cfc231d3b8798cadf
jf-guillou <jean.f.guillou@gmail.com>
2018-03-19 13:50:25 +0100

    Add proxy support

--- waptrepo.py
+++ waptrepo.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import waptpackage
+import os
 
 REPOS = {
     'tis': {'url': 'https://wapt.tranquil.it/wapt', 'repo': None},
@@ -16,6 +17,6 @@ def get_local_repo():
 def get_remote_repos():
     """Get remote package repositories and init connection"""
     for name, rep in REPOS.items():
-        rep['repo'] = waptpackage.WaptRemoteRepo(name=name, url=rep['url'], timeout=4)
+        rep['repo'] = waptpackage.WaptRemoteRepo(name=name, url=rep['url'], timeout=4, http_proxy=os.environ.get('https_proxy'))
         rep['repo'].verify_cert = True
     return REPOS
